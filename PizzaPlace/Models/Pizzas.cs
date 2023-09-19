c40d7f1921a3e6b6de628e93d5c8613d43bd832c
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-19 18:04:54 -0400

    Agregando Clase Pizza

---
+++ PizzaPlace/Models/Pizzas.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaPlace.Models
+{
+    public class Pizzas
+    {
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public Picante Picante { get; }
+        public Pizzas(int id, string  nombre, decimal precio, Picante picante)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Picante = picante;
+        }
+    }
+}
