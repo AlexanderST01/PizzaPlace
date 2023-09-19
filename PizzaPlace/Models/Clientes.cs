bd4b57d91c10635584eeb8f4e5244234b9e8ee6d
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-19 18:05:44 -0400

    Agregnado Clase Cliente

---
+++ PizzaPlace/Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Por favor provee un nombre")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Por favor provee una calle")]
+        public string? Calle { get; set; }
+        [Required(ErrorMessage = "Por favor provee una ciudad")]
+        public string? Ciudad { get; set; }
+
+    }
+}
