0ad7ee339ff01e89837c692399cad38f862aca91
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-19 18:06:24 -0400

    Agregando clase CanastaCompras

---
+++ PizzaPlace/Models/CanastaCompras.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaPlace.Models
+{
+    public class CanastaCompras
+    {
+        public Clientes Clientes { get; set; } = new();
+        public List<int> Ordenes { get; set; } = new();
+        public bool HaPagodo { get; set; }
+        public void Agregar(int pizzaId) => Ordenes.Add(pizzaId);
+        public void Remover(int pos) => Ordenes.RemoveAt(pos);
+    }
+}
