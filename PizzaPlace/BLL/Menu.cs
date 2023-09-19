58a31653f07261ce024b06e2ffb3c6fee11be4bc
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-19 18:05:26 -0400

    Agregando clase Menu

---
+++ PizzaPlace/BLL/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using PizzaPlace.Models;
+
+namespace PizzaPlace.BLL
+{
+    public class Menu
+    {
+        public List<Pizzas> Pizzas { get; set; } = new List<Pizzas>();
+        public void Add(Pizzas pizzas) 
+        => Pizzas.Add(pizzas);
+        public Pizzas? OptenerPizzas(int id) 
+        => Pizzas.SingleOrDefault(Pizzas => Pizzas.Id == id);
+    }
+}
