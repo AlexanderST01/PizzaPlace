c7af7ec176f55b9c6053286ece6fba1f5b12cbda
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-19 18:07:31 -0400

    Agregando clase Estados

---
+++ PizzaPlace/Models/Estados.cs
@@ -0,0 +1,11 @@
+﻿using PizzaPlace.BLL;
+namespace PizzaPlace.Models
+{
+    public class Estados
+    {
+        public Menu Menu { get; set; } = new Menu();
+        public CanastaCompras CanastaCompras { get; set; } = new CanastaCompras();
+        public InterfasUsuario InterfasUsuario { get; set;} = new InterfasUsuario();
+        public decimal PrecioTotal => CanastaCompras.Ordenes.Sum(id => Menu.OptenerPizzas(id)!.Precio);
+    }
+}
