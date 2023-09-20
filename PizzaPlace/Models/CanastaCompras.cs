091d319988f4b099b47df320a5e244666f2bc704
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-20 00:22:29 -0400

    Cambiando nombre de variable en CanastaCompra

--- PizzaPlace/Models/CanastaCompras.cs
+++ PizzaPlace/Models/CanastaCompras.cs
@@ -4,7 +4,7 @@
     {
         public Clientes Clientes { get; set; } = new();
         public List<int> Ordenes { get; set; } = new();
-        public bool HaPagodo { get; set; }
+        public bool HaPagado { get; set; }
         public void Agregar(int pizzaId) => Ordenes.Add(pizzaId);
         public void Remover(int pos) => Ordenes.RemoveAt(pos);
     }
